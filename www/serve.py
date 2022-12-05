54caddba0d7b921514b118cfe1180e497da052a8
Steven Larsen <stevenlarsen@Stevens-Mini.attlocal.net>
2022-12-04 18:39:06 -0600

    pushed headers to root

--- www/serve.py
+++ www/serve.py
@@ -7,11 +7,6 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
-        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET')
-        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         return super(CORSRequestHandler, self).end_headers()
 
 
